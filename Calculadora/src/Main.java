9e9cafa82e04ca164f79ab1abb158cdc9dc081d5
Rodrigo Hay <hay.rodrigo@gmail.com>
2019-12-03 21:38:21 +0000

    Mudei as variáveis

--- Calculadora/src/Main.java
+++ Calculadora/src/Main.java
@@ -1,9 +1,27 @@
+import java.util.Scanner;
+
 
 public class Main {
 
+	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int resposta = 0;
+		String opcao = "";
+		
+		Scanner stdIn = new Scanner(System.in);
 
+		System.out.println("Insira o primeiro valor a ser calculado:");
+		resposta = stdIn.nextInt();
+		
+		switch (opcao)  {
+		case "x":
+			break;
+		
+		}
+		
+		
 	}
 
 }
